7c150b13b213cfea0f09a1284c7afc7d546427ec
paire-nature <paire-nature@live.fr>
2015-09-12 16:34:35 +0100

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -8,7 +8,7 @@
 // ==/UserScript==
 
 $(function() { 
-   $('').each(function() {
+   $('.ligne0 , .ligne1').each(function() {
 	      if ($(this).html().match(/vostfr/) ) {
          $(this).remove();
 		 }
